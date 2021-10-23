5d00cc93d94c06dfe3e9efdfb1e63efa2479e702
infor-ujjval <ujjval.info@gmail.com>
2021-10-23 09:25:57 -0400

    Initial Document

--- server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-// web-ui will be the same folder name.
-app.use(express.static("web-ui")); 
-app.get('/', function (req, res,next) {
- res.redirect('/'); 
-});
-app.listen(4200, 'localhost');
-console.log("Server is Listening on port 4200");
