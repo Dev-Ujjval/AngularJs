a790bc415cf21ff4ce8fe7f7047cd334fe3b67f9
infor-ujjval <ujjval.info@gmail.com>
2021-10-23 09:21:16 -0400

    Service Separation

---
+++ web-ui/github.service.js
@@ -0,0 +1,27 @@
+(function() {
+
+    var github = function($http) {
+
+        var getuser = function(username) {
+            return $http.get("https://api.github.com/users/" + username)
+                            .then(function(response){
+                                return response.data;
+                            }) ;
+        };
+
+        var getRepos = function(user) {
+                return $http.get(user.repos_url)
+                            .then(function(response){
+                                return response.data;
+                            });
+        }
+
+        return {
+            getuser: getuser, 
+            getRepos: getRepos
+        };
+    };
+
+    var module = angular.module("five");
+    module.factory("github", github)
+}());
--- web-ui/script.js
+++ web-ui/script.js
@@ -2,21 +2,20 @@
 
 (function () {
 
-  var app = angular.module('fourth', []);
+  var app = angular.module('five', []);
 
-  var MainCtrl = function ($scope, $http) {
+  var MainCtrl = function ($scope, github) {
     $scope.username = "dev-ujjval";
     $scope.message = "Hello, Angular!";
     $scope.repos_orderby = "+stargazers_count";
 
-    var onUserComplete = function (response) {
-      $scope.user = response.data;
-      $http.get($scope.user.repos_url)
-            .then(onRepos, onError);
+    var onUserComplete = function (data) {
+      $scope.user = data;
+      github.getRepos($scope.user).then(onRepos, onError);
     }
 
-    var onRepos = function(response) {
-      $scope.repos = response.data;
+    var onRepos = function(data) {
+      $scope.repos = data;
     }
 
     var onError = function (reason) {
@@ -24,13 +23,12 @@
     }
 
     $scope.search = function() {
-      $http.get("https://api.github.com/users/" + $scope.username)
-      .then(onUserComplete, onError);  
+      github.getuser($scope.username).then(onUserComplete, onError);  
     }
 
   }
 
-  app.controller("MainCtrl", ["$scope", "$http", MainCtrl]);
+  app.controller("MainCtrl", ["$scope", "github", MainCtrl]);
 
 }());
 
